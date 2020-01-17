39d32a1a5047b171a2b8bc0c98c0d6f13d33a6f1
Mark <mzamora@aptude.com>
2020-01-17 15:09:16 -0600

    actions created

--- src/redux/pokemonsDuck.js
+++ src/redux/pokemonsDuck.js
@@ -1,18 +1,41 @@
+import axios from 'axios'
+
 //Constants
 let initialData = {
-    pokemons: []
+    pokemon: {},
+    pokemons: [],
+    exist: false,
+    message: ""
 }
 
-let GET_POKEMONS = "GET_POKEMONS"
-
+let GET_POKEMON = "GET_POKEMON"
+let GET_POKEMON_ERROR = "GET_POKEMON"
 
 //Reducer
 export default function reducer(state = initialData, action) {
     switch(action.type) {
-        case GET_POKEMONS:
+        case GET_POKEMON:
+            return {...state, pokemon: action.payload, exist: true}
+        case GET_POKEMON_ERROR:
+            return {...state, message: action.payload, exist: false}
         default:
             return state
     }
 }
 
-//Actions
+//Actions creators (thunk)
+export let getPokemonsAction = (pokemon) => (dispatch, getState) => {
+    return axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon}/`)
+                .then(response => {
+                    dispatch({
+                        type: GET_POKEMON,
+                        payload: response.data
+                    })
+                })
+                .catch(error => {
+                    dispatch({
+                        type: GET_POKEMON_ERROR,
+                        payload: error.response.data
+                    })
+                })
+}
