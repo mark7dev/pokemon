99f399da6f67e3111df84c85474fcfe333ea3be5
Mark <mzamora@aptude.com>
2020-01-17 12:42:25 -0600

    store created

---
+++ src/redux/store.js
@@ -0,0 +1,14 @@
+import { createStore, combineReducers, compose, applyMiddleware } from 'redux'
+import pokemonsReducer from './pokemonsDuck'
+import thunk from 'redux-thunk'
+
+let rootReducer = combineReducers({
+    pokemons: pokemonsReducer
+})
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export default function generateStore() {
+    let store = createStore(rootReducer, composeEnhancers( applyMiddleware(thunk)) )
+    return store
+}
