5a29291323c27c52ac57be3d33d431f3ad1164f4
Mark <mzamora@aptude.com>
2020-01-17 12:52:34 -0600

    app connected to Redux by provider

--- src/index.js
+++ src/index.js
@@ -3,8 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux'
+import generateStore from './redux/store'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let store = generateStore()
+
+let WithStore = () => <Provider store={store}><App /></Provider>
+
+ReactDOM.render(<WithStore />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
