0e80427cf27c7180ad63c5dc6f1f45188011437d
mark7dev <mark7dev@hotmail.com>
2020-01-19 01:28:58 -0600

    clean code

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,58 +1,33 @@
 import React, { Fragment, useState } from 'react'
 import { connect } from 'react-redux'
-import { getPokemonsAction, addPokemonAction } from '../redux/pokemonsDuck'
+import { addPokemonAction } from '../redux/pokemonsDuck'
 
-function Home({pokemons, getPokemonsAction, addPokemonAction}) {
+function Home({pokemons, addPokemonAction}) {
 
     const [homeState, saveHomeState] = useState({
         nameId: '',
         disabledBtn: true
     })
 
-
-    const getPoks = () => {
-
-        let param = homeState.nameId
-        // getPokemonsAction(param)
+    const getPokemon = () => {
+        let param = homeState.nameId.toLowerCase()
         addPokemonAction(param)
-        // console.log(param);
         saveHomeState({nameId: ''})
         document.getElementById("nameId").value = ""
-        // disabledBtn()
+        if(pokemons.exist === false) {
+            alert('ERROR!!')
+        }
     }
-
-    const test = () => {
-        let name = homeState.nameId
-        console.log(name);
-    }
-
-    // const disabledBtn = () => {
-    //     if(homeState.nameId === '') {
-    //         homeState.disabledBtn = true
-    //     } else {
-    //         homeState.disabledBtn = false
-    //     }
-    // }
-
-    // function handleChange(e) {
-    //     e.preventDefault();
-    //     console.log(e.target.value);
-    //     let pokemon = e.target.value
-    //     return pokemon
-    // }
 
     const handleChange = e => {
-        e.preventDefault();
-        saveHomeState({
-            nameId: e.target.value
-        })
+        e.preventDefault();      
+        saveHomeState({nameId: e.target.value})
     }
 
     return (
         <Fragment>
             <input id="nameId" type="text" onChange={handleChange}></input>
-            <button onClick={getPoks}>CATCH POKEMON!</button>
-            <button onClick={test}>TEST</button>
+            <button onClick={getPokemon} disabled={!homeState.nameId}>CATCH POKEMON!</button>
         </Fragment>
     )
 }
@@ -63,7 +38,7 @@ function mapState({ pokemons }) {
     }
   }
 
-  export default connect( mapState, {getPokemonsAction, addPokemonAction})(Home)
+export default connect( mapState, {addPokemonAction})(Home)
 
 
 
--- src/redux/pokemonsDuck.js
+++ src/redux/pokemonsDuck.js
@@ -2,21 +2,17 @@ import axios from 'axios'
 
 //Constants
 let initialData = {
-    pokemon: {},
-    pokemons: [],
+    array: [],
     exist: false,
     message: ""
 }
 
-let GET_POKEMON = "GET_POKEMON"
-let GET_POKEMON_ERROR = "GET_POKEMON"
+let GET_POKEMON_ERROR = "GET_POKEMON_ERROR"
 let ADD_POKEMON = "ADD_POKEMON"
 
 //Reducer
 export default function reducer(state = initialData, action) {
     switch(action.type) {
-        case GET_POKEMON:
-            return {...state, pokemon: action.payload, exist: true}
         case GET_POKEMON_ERROR:
             return {...state, message: action.payload, exist: false}
         case ADD_POKEMON:
@@ -32,30 +28,11 @@ export let addPokemonAction = (pokemon) => (dispatch, getState) => {
                 .then(response => {
                     console.log(response);
                     let newPokemon = response.data
-                    let pokemons = getState().pokemons.pokemons
-                    pokemons.push(newPokemon)
+                    let pokemonsArray = getState().pokemons.array
+                    pokemonsArray.push(newPokemon)
                     dispatch({
                         type: ADD_POKEMON,
-                        payload: {pokemons: [...pokemons]}
-                    })
-                })
-                .catch(error => {
-                    console.log(error);
-                    dispatch({
-                        type: GET_POKEMON_ERROR,
-                        payload: error.response.data
-                    })
-                })
-}
-
-//Actions creators (thunk)
-export let getPokemonsAction = (pokemon) => (dispatch, getState) => {
-    return axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon}/`)
-                .then(response => {
-                    console.log(response);
-                    dispatch({
-                        type: GET_POKEMON,
-                        payload: response.data
+                        payload: {array: [...pokemonsArray]}
                     })
                 })
                 .catch(error => {
